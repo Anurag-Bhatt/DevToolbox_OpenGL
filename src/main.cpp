f3f4321a849afd26b0eb3f8b48722b21dd2f1d0a
Anurag-Bhatt <anuragbhatt865@gmail.com>
2025-06-09 22:00:04 +0530

    Implemented phong lightning and mouse toggling

--- src/main.cpp
+++ src/main.cpp
@@ -31,42 +31,48 @@ int main() {
 
     std::cout << "Current Working Directory: " << std::filesystem::current_path() << std::endl;
 
-    WindowManager window(SCREEN_WIDTH, SCREEN_HEIGHT, std::string("Raytracer"));
+    WindowManager window(SCREEN_WIDTH, SCREEN_HEIGHT, std::string("I dont know"));
     
-    std::vector<float> vertices = {
-            // position         // uv
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // 0
-         0.5f, -0.5f, -0.5f,  1.0f, 0.0f, // 1
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // 2
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // 3
+    std::vector<float> cubeVertices = {
+    // positions           // uvs     // normals (back face)
+    -0.5f, -0.5f, -0.5f,   0.0f, 0.0f,   0.0f,  0.0f, -1.0f, // 0
+     0.5f, -0.5f, -0.5f,   1.0f, 0.0f,   0.0f,  0.0f, -1.0f, // 1
+     0.5f,  0.5f, -0.5f,   1.0f, 1.0f,   0.0f,  0.0f, -1.0f, // 2
+    -0.5f,  0.5f, -0.5f,   0.0f, 1.0f,   0.0f,  0.0f, -1.0f, // 3
 
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // 4
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // 5
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // 6
-        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, // 7
+    // front face
+    -0.5f, -0.5f,  0.5f,   0.0f, 0.0f,   0.0f,  0.0f,  1.0f, // 4
+     0.5f, -0.5f,  0.5f,   1.0f, 0.0f,   0.0f,  0.0f,  1.0f, // 5
+     0.5f,  0.5f,  0.5f,   1.0f, 1.0f,   0.0f,  0.0f,  1.0f, // 6
+    -0.5f,  0.5f,  0.5f,   0.0f, 1.0f,   0.0f,  0.0f,  1.0f, // 7
 
-        -0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // 8
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // 9
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // 10
-        -0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // 11
+    // left face
+    -0.5f,  0.5f,  0.5f,   1.0f, 1.0f,  -1.0f,  0.0f,  0.0f, // 8
+    -0.5f,  0.5f, -0.5f,   0.0f, 1.0f,  -1.0f,  0.0f,  0.0f, // 9
+    -0.5f, -0.5f, -0.5f,   0.0f, 0.0f,  -1.0f,  0.0f,  0.0f, //10
+    -0.5f, -0.5f,  0.5f,   1.0f, 0.0f,  -1.0f,  0.0f,  0.0f, //11
 
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // 12
-         0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // 13
-         0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // 14 
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // 15
+    // right face
+     0.5f,  0.5f,  0.5f,   1.0f, 1.0f,   1.0f,  0.0f,  0.0f, //12
+     0.5f,  0.5f, -0.5f,   0.0f, 1.0f,   1.0f,  0.0f,  0.0f, //13
+     0.5f, -0.5f, -0.5f,   0.0f, 0.0f,   1.0f,  0.0f,  0.0f, //14
+     0.5f, -0.5f,  0.5f,   1.0f, 0.0f,   1.0f,  0.0f,  0.0f, //15
 
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // 16
-         0.5f, -0.5f, -0.5f,  1.0f, 1.0f, // 17
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // 18
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // 19
+    // bottom face
+    -0.5f, -0.5f, -0.5f,   0.0f, 1.0f,   0.0f, -1.0f,  0.0f, //16
+     0.5f, -0.5f, -0.5f,   1.0f, 1.0f,   0.0f, -1.0f,  0.0f, //17
+     0.5f, -0.5f,  0.5f,   1.0f, 0.0f,   0.0f, -1.0f,  0.0f, //18
+    -0.5f, -0.5f,  0.5f,   0.0f, 0.0f,   0.0f, -1.0f,  0.0f, //19
 
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // 20
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // 21
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // 22
-        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f  // 23
-    };
+    // top face
+    -0.5f,  0.5f, -0.5f,   0.0f, 1.0f,   0.0f,  1.0f,  0.0f, //20
+     0.5f,  0.5f, -0.5f,   1.0f, 1.0f,   0.0f,  1.0f,  0.0f, //21
+     0.5f,  0.5f,  0.5f,   1.0f, 0.0f,   0.0f,  1.0f,  0.0f, //22
+    -0.5f,  0.5f,  0.5f,   0.0f, 0.0f,   0.0f,  1.0f,  0.0f  //23
+};
 
-    std::vector<unsigned int> indices = {
+
+    std::vector<unsigned int> cubeIndices = {
         // back face
         0, 1, 2, 2, 3, 0,
         // front face
@@ -134,16 +140,15 @@ int main() {
     Texture tex1("../assets/images/container.jpg");
     Texture tex2("../assets/images/awesomeface.png");
  
-    Shader myShader("../shaders/screen.vert", "../shaders/raytrace.frag");
+    Shader cubeShader("../shaders/screen.vert", "../shaders/raytrace.frag");
     Shader lightShader("../shaders/lightSource.vert", "../shaders/lightSource.frag");
 
-    auto sharedCubeMesh = std::make_shared<Mesh>(vertices, indices);
-    sharedCubeMesh->setLayout({3, 2});
+    auto sharedCubeMesh = std::make_shared<Mesh>(cubeVertices, cubeIndices);
+    sharedCubeMesh->setLayout({3, 2, 3});
 
     Object cube(sharedCubeMesh);
     cube.setTransform(glm::vec3(0.0,0.0,0.0));
 
-    // TODO: work on light
 
     auto shareLightMesh = std::make_shared<Mesh>(lightCubeVertices, lightCubeIndices);
     shareLightMesh->setLayout({3});
@@ -152,42 +157,59 @@ int main() {
     lightSource.setTransform(glm::vec3(0.0, 4.0, 0.0));
 
     Camera cam;
-    glfwSetInputMode(window.getWindow(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    
 
     glm::mat4 model = glm::mat4(1.0f);
     model = glm::translate(model, glm::vec3(0.0,0.0,0.0));
 
+    glm::mat4 lightModel = glm::mat4(1.0f);
+    lightModel = glm::translate(lightModel,lightSource.getPosition());
+
     while (window.shoulBeOpen()) {
         
-        // std::cout << window.getDeltaTime() << " is the deltaTime\n";
-        //window.processInput();
         window.clearBuffer(bgColor); 
         window.beginUIFrame();
-        cam.processKeyboardInput(window.getWindow(), window.getDeltaTime());
-        cam.processMouseInput(window.getWindow());
         
-        myShader.use();
-        myShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
-        myShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
         
-        myShader.setMat4("view", cam.lookAt());
-        myShader.setMat4("projection", glm::perspective(glm::radians(45.0f), window.getAspectRatio(), 0.1f, 100.0f));
+        // ImGui UI, for helping in debugging
+        if(window.showUI){
+            static float lightPosition[3] = { 0.0f, 1.0f, 0.0f }; // default value
+            ImGui::InputFloat3("Light Position", lightPosition);
+
+            // Reset to identity before applying the translation
+            lightModel = glm::mat4(1.0f);
+            lightModel = glm::translate(lightModel, glm::vec3(lightPosition[0], lightPosition[1], lightPosition[2]));
+        }
+
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), window.getAspectRatio(), 0.1f, 100.0f);
+
+        // Cube Shader and Cube Source
+        cubeShader.use();
+        cubeShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
+        cubeShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+        cubeShader.setVec3("lightPosition", lightSource.getPosition());
+        cubeShader.setVec3("viewPosition", cam.getPosition());
+        cubeShader.setMat4("view", cam.lookAt());
+        cubeShader.setMat4("projection", projection);
+        
         tex1.bind(0);
         tex2.bind(1);        
-        myShader.setInt("texture1", 0);
-        myShader.setInt("texture2", 1);
+        
+        cubeShader.setInt("texture1", 0);
+        cubeShader.setInt("texture2", 1);
+        
         model = glm::translate(model, cube.getPosition());
-        myShader.setMat4("model", model);
+        cubeShader.setMat4("model", model);
         cube.draw();
 
         tex1.unbind();
         tex2.unbind();
 
+        // Light source and Light Shader
         lightShader.use();
+        
         lightShader.setMat4("view", cam.lookAt());
-        lightShader.setMat4("projection",glm::perspective(glm::radians(45.0f), window.getAspectRatio(), 0.1f, 100.0f));
-        glm::mat4 lightModel = glm::mat4(1.0f);
-        lightModel = glm::translate(lightModel,lightSource.getPosition());
+        lightShader.setMat4("projection", projection);
         lightModel = glm::scale(lightModel, glm::vec3(0.2f));
         lightShader.setMat4("model", lightModel);
 
@@ -196,6 +218,12 @@ int main() {
 
         window.renderUI();
         window.pollEvents();
+
+        // Processing Camera Input
+        cam.processKeyboardInput(window.getWindow(), window.getDeltaTime());
+        cam.mouseControlEnabled = window.toggleMouseControl;
+        cam.processMouseInput(window.getWindow());
+
         window.swapBuffers();
     }
 
